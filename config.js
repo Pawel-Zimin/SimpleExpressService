8c1239ce7fcf2d7533893d6603bc48688114537e
Pawel Zimin <paweu14@gmail.com>
2020-09-18 13:55:25 +0200

    adds login page and redirect mechanism for it

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+   keySession: ['klucz'],
+   maxAgeSession: 24 * 60 * 60 * 1000,
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+router.all('*', (req, res, next) => {
+   if(!req.session.admin){
+      res.redirect('login');
+      return;
+   }
+
+   next();
+});
+
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('admin', { title: 'Admin' });
+   console.log(req.session.admin);
+
+   res.render('admin', { title: 'Admin' });
 });
 
 module.exports = router;
